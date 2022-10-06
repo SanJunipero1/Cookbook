4f1d35c3e20ca58d04600a1ad63d62ad6593d513
SanJunipero1 <110401109+SanJunipero1@users.noreply.github.com>
2022-10-06 18:16:20 +0200

    update10

--- js/app.js
+++ js/app.js
@@ -4,3 +4,21 @@ function rezeptTeilen(){
     alert("Rezept wird geteilt")
 }
 button.addEventListener("click",rezeptTeilen)
+
+const rezepte = [
+    {
+        name:"Pizza",
+        kochdauer:30,
+        beschreibung:"Der einzige Unterschied zu den unzähligen Rezepten besteht eigentaten immer ",
+        zutaten:[
+            {name:"Tomate",stueckzahl:2},
+            {name:"Kaese",stueckzahl:4},
+            {name:"Salami",stueckzahl:5},
+            {name:"Pilze",stueckzahl:2},
+            {name:"Schinken",stueckzahl:3}
+            
+        ],
+        author:"Max",
+        bild:"src=jpg"
+    }
+]
